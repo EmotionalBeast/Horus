6deb45612112dfaf4fc157f8f1c6ce0f40fa9afd
Jhin Yao <jhinyao@163.com>
2020-02-19 19:04:05 +0800

    bugfix

--- Start.py
+++ Start.py
@@ -1,80 +1,80 @@
-#!/usr/local/bin/python3
+##!/usr/local/bin/python3
 # coding: utf-8
 
+'''
+写入格式属性控制：在Formatting.py中
+font 
+alignment 
+borders
+pattern 
+protection
+'''
 import xlwt, os, time
 from picture import Picture
 from video import Video
 
 PIC_FORMAT = ("png", "jpg", "jpeg")
-VID_FORMAT = ("mp4","mov")
+VID_FORMAT = ("mp4","mov", "MOV")
 PIC_SHEET_TITLE = ["图片名称", "分辨率", "大小(MB)", "DPI"]
 VID_SHEET_TITLE = ["视频名称", "分辨率", "大小(MB)", "视频时长","码率(KBPS)", "帧率(FPS)"]
 
+#控制输出格式
+def setStyle():
+	style = xlwt.XFStyle()
+	#对齐方式
+	alignment = xlwt.Alignment()
+	alignment.horz = alignment.HORZ_CENTER
+	alignment.vert = alignment.VERT_CENTER
+	style.alignment = alignment
+	return style
+
+#写入文件
 def write_to_excel(path, *info):
-	# #设置字体
-	# font = xlwt.Font()
-	# font.name = "宋体"
-	# font.color = "black"
-	# font.height = "340"
-
-	# #设置居中
-	# alignment = xlwt.Alignment()
-	# alignment.horz = xlwt.Alignment.HORZ_CENTER
-	# alignment.vert = xlwt.Alignment.VERT_CENTER
-
-	# #设置边框
-	# borders = xlwt.Borders()
-	# borders.left = xlwt.Borders.THIN
-	# borders.right = xlwt.Borders.THIN
-	# borders.top = xlwt.Borders.THIN
-	# borders.bottom = xlwt.Borders.THIN
-
-	# #设置背景颜色
-	# pattern = xlwt.Pattern()
-	# pattern.pattern = xlwt.Pattern.SOLID_PATTERN
-	# pattern.pattern_fore_colour = 3
-
-	# #定义style
-	# style = xlwt.XFStyle()
-	# style.font = font
-	# style.borders = borders
-	# style.alignment = alignment 
-
 	workbook = xlwt.Workbook(encoding="utf-8")
 	sheet = workbook.add_sheet("sheet1")
 	for i in range(len(info)):
 		for j in range(len(info[i])):
-			sheet.write(i, j, info[i][j])
+			sheet.write(i, j, info[i][j], setStyle())
+	sheet.col(0).width = 256 * 50
+	for i in range(len(info[0])):
+		if i != 0:
+			sheet.col(i).width = 256 * 20
 	workbook.save(path)
 	print("表格写入数据成功！")
 
+def sort_by_creation_time(files):
+	for i in range(len(files)-1):
+		for j in range(len(files)-i-1):
+			if os.path.getctime(files[j+1]) > os.path.getctime(files[j]):
+				files[j], files[j+1] = files[j+1], files[j]
+	return files
+
+#得到文件中关键信息
 def get_info_list(path):
 	workspace_path = path + "/workspace"
 	pic_info = []
 	pic_info.append(PIC_SHEET_TITLE)
 	vid_info = []
 	vid_info.append((VID_SHEET_TITLE))
-	dir_path_list = []
-	for root, dirs, files in os.walk(workspace_path):
-		for dir in dirs:
-			tmp = root + "/" + dir
-			dir_path_list.append(tmp)
+	target_files = []
 
-	for i in range(len(dir_path_list)):
-		for root, dirs, files in os.walk(dir_path_list[i]):
-			for file in files:
-				tmp_file = root + "/" + file
-				_, tmp_suffix = file.split(".")
-				if tmp_suffix in PIC_FORMAT:
-					pic = Picture(tmp_file)
-					pic_item = pic.get_info()
-					pic_info.append(pic_item)
-				elif tmp_suffix in VID_FORMAT:
-					vid = Video(tmp_file)
-					vid_item = vid.get_info()
-					vid_info.append(vid_item)
-				else:
-					continue
+	for root, dirs, files in os.walk(workspace_path):
+		for file in files:
+			tmp_file = root + "/" + file
+			target_files.append(tmp_file)
+
+	for file in sort_by_creation_time(target_files):
+		_, file_suffix = file.split(".")
+		if file_suffix in PIC_FORMAT:
+			pic = Picture(file)
+			pic_item = pic.get_info()
+			pic_info.append(pic_item) #二维列表
+		elif file_suffix in VID_FORMAT:
+			vid = Video(file)
+			vid_item = vid.get_info()
+			vid_info.append(vid_item)
+		else:
+			continue
 	return pic_info, vid_info
 
 def print_info(*info):
@@ -93,10 +93,12 @@ if __name__ == "__main__":
 
 	pic_xls_path = path + "/workspace/picture_info_" + timeStr + ".xls"
 	vid_xls_path = path + "/workspace/video_info_" + timeStr + ".xls"
+	
 	if len(pic_info) != 0:
 		write_to_excel(pic_xls_path, *pic_info)
 	else:
 		print("没有图片文件")
+
 	if len(vid_info) != 0:
 		write_to_excel(vid_xls_path, *vid_info)
 	else:
--- test.py
+++ test.py
@@ -3,52 +3,67 @@
 from PIL import Image
 import cv2, os, time
 
-img = Image.open("./workspace/view.jpg")
-print(os.path.getsize("./workspace/view.jpg")/(1000*1000))
+# img = Image.open("./workspace/view.jpg")
+# print(os.path.getsize("./workspace/view.jpg")/(1000*1000))
 
-print(img.size)
-print(img.format)
-# print(img.info)
-dpi = img.info["dpi"]
-print(img.info["dpi"])
-print()
+# print(img.size)
+# print(img.format)
+# # print(img.info)
+# dpi = img.info["dpi"]
+# print(img.info["dpi"])
+# print()
 
-#码率计算公式：file_size(kb)*8/time(s)
-"""
-get(self, propId)
-propId:
-cv2.CAP_PROP_POS_MSEC 视频
-cv2.CAP_PROP_FPS 帧率
-cv2.CAP_PROP_FRAME_COUNT 帧数
-cv2.CAP_PROP_FRAME_WIDTH 帧的高度
-cv2.CAP_PROP_FRAME_HEIGHT 帧的宽度
-"""
-cap = cv2.VideoCapture("./workspace/ViewRecorder_tmp.mp4")
-print(cap.get(cv2.CAP_PROP_FPS))
-print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# #码率计算公式：file_size(kb)*8/time(s)
+# """
+# get(self, propId)
+# propId:
+# cv2.CAP_PROP_POS_MSEC 视频
+# cv2.CAP_PROP_FPS 帧率
+# cv2.CAP_PROP_FRAME_COUNT 帧数
+# cv2.CAP_PROP_FRAME_WIDTH 帧的高度
+# cv2.CAP_PROP_FRAME_HEIGHT 帧的宽度
+# """
+# cap = cv2.VideoCapture("./workspace/ViewRecorder_tmp.mp4")
+# print(cap.get(cv2.CAP_PROP_FPS))
+# print(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+# print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# img = cv2.imread("./workspace/view.jpg", 1)
-# cv2.imwrite("./workspace/view_3.jpg",img)
-video_duration = float(cap.get(7)/cap.get(5)) #视频时长
-print(video_duration)
-def bit(path):
-	size = os.path.getsize(path)
-	print(size/1024/1024)
-	bit_rate = size/1024*8/video_duration
-	print(bit_rate)
-	print()
+# # img = cv2.imread("./workspace/view.jpg", 1)
+# # cv2.imwrite("./workspace/view_3.jpg",img)
+# video_duration = float(cap.get(7)/cap.get(5)) #视频时长
+# print(video_duration)
+# def bit(path):
+# 	size = os.path.getsize(path)
+# 	print(size/1024/1024)
+# 	bit_rate = size/1024*8/video_duration
+# 	print(bit_rate)
+# 	print()
 
-bit("./workspace/ViewRecorder_tmp.mp4")
-bit("./workspace/AnimatedStory.mov")
-bit("./workspace/IMG_1348.mp4")
-content = 'a.v'
-print(len(content))
+# bit("./workspace/ViewRecorder_tmp.mp4")
+# bit("./workspace/AnimatedStory.mov")
+# bit("./workspace/IMG_1348.mp4")
+# content = 'a.v'
+# print(len(content))
 
-print(time.time())
-print(time.localtime(time.time()))
-print(time.strftime('%Y_%m_%d_%H_%M', time.localtime(time.time())))
+# print(time.time())
+# print(time.localtime(time.time()))
+# print(time.strftime('%Y_%m_%d_%H_%M', time.localtime(time.time())))
 
 
 
+
+file = os.getcwd() + "/workspace"
+for root, dirs, files in os.walk(file):
+	for file in files:
+		tmp = root + "/" + file
+		print(tmp)
+		print("mtime:", os.path.getmtime(tmp))
+		print("ctime:", os.path.getctime(tmp))
+		print()
+
+# l1 = [1,3,4]
+# tmp = l1[0]
+# l1[0] = l1[1]
+# l1[2] = tmp
+# print(l1)
